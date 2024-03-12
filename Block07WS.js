6adbf8eed68c24b006863aaa4939b0a0f0452ce5
Jordyn <97773417+JMSONNE@users.noreply.github.com>
2024-03-11 20:14:39 -0500

    Update Block07WS.js

--- Block07WS.js
+++ Block07WS.js
@@ -5,7 +5,7 @@ const thirdNumber =  40 - 1; //this equals 39, the final number
 
 //Here I give the user the message
 
-console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
 
 //And here is where I concantenate the variables with dashes to display to the user
 
